92f188b84bb8d55796d78f14b047527e7afad305
Adrian <adlvasqu@ucsc.com>
2021-04-07 14:41:14 -0700

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+let config = {
+    type: Phaser.CANVAS,
+    width:640,
+    height:480,
+    scene: [Play, Menu],
+}
+let game = new Phaser.Game(config);
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
