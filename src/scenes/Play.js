cec51f420a586c1c34ef08a1684262a51714c052
Adrian <adlvasqu@ucsc.com>
2021-04-12 04:51:51 -0700

    Added P1 rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,10 +4,15 @@ class Play extends Phaser.Scene {
     }
     preload(){
         this.load.image('starfield', 'assets/starfield.png');
+        this.load.image('rocket', 'assets/rocket.png');
     }
     create(){
         // green UI background
         this.starfield = this.add.tileSprite(0,0,640,480,'starfield').setOrigin(0,0);
+
+        this.p1rocket = new Rocket(this, game.config.width/2, 
+                                   game.config.height - borderUISize - 
+                                   borderPadding, 'rocket');
 
         this.add.rectangle(0, borderUISize + borderPadding, 
                            game.config.width, borderUISize * 2, 
@@ -18,8 +23,14 @@ class Play extends Phaser.Scene {
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
     update(){
         this.starfield.tilePositionX -= 4;
+
+        this.p1rocket.update();
     }
 }
