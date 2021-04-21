0a9fa7cd297456eb713bbe6504e05af3e5d2bbce
Adrian <adlvasqu@ucsc.com>
2021-04-21 16:36:33 -0700

    formalities

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+// Adrian Vasquez
+// 4/21/2020
+// Etimated time to completions: 12-15hrs.
+
+/*
+* I did both of the S tier options: changed the theme to a pizza delivery game
+* I made all original sounds and drawings: (60 pts)
+*
+* I made a simoltanious 2 player mode where two seperate scores are displayed, and
+* the winner is displayed when the game is over. Single player is still available:
+* (30pts)
+*
+* I made a countdown clock that shows the player/players how much time the have left:
+* (10pts)
+*
+* 60pts + 30pts + 10pts = 100pts
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width:640,
